0e71ea4ff6a7ab70e9e232e0c040cc6bf42aaf6d
Yuta Hinokuma <yuta.hinokuma725@gmail.com>
2021-05-01 01:40:00 +0900

    3-4-1を実装する

---
+++ chapter3/src/3_4_1/main.rs
@@ -0,0 +1,26 @@
+use std::io::{stdin, Read, Error, ErrorKind};
+
+fn main() {
+    loop {
+        let mut buffer = vec![0; 5];
+
+        match stdin().read(&mut buffer) {
+            Ok(size) if size == 0 => {
+                println!("EOF");
+                break;
+            }
+            Ok(size) => {
+                println!("size={} input'{}'", size, std::str::from_utf8(&buffer).unwrap());  // rustはStringがUTF-8だから若干違うのはしょうがない？
+            }
+            Err(e) => {
+                if e.kind() == ErrorKind::UnexpectedEof {
+                    println!("EOF");
+                    break;
+                }
+                else {
+                    unreachable!();
+                }
+            }
+        }
+    }
+}
--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpStream;
+use std::io::{Write, Read, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let mut conn = TcpStream::connect("ascii.jp:80").unwrap();
+
+    conn.write("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n".as_bytes()).unwrap();
+
+    BufReader::new(conn);
+    conn.read(&mut buf);
 }
