9a4d3bd6d312585509d1f726ae3e731d685af24a
saffron <y.saffrontea@gmail.com>
2021-05-01 18:15:09 +0900

    add 3_2_2_1

---
+++ chapter3/src/main_3_2_2_1.rs
@@ -0,0 +1,18 @@
+/**
+ * 
+ * chapter 3_2_2_1
+ * io.copy
+ * 
+ */
+use std::io::copy;
+
+pub fn main() -> std::io::Result<()>{
+    let mut reader : &[u8] = b"hello!";
+    let mut buffer : Vec<u8> = vec![];
+  match copy(&mut reader,&mut buffer){
+        Ok(result) => result,
+        Err(err) => {panic!("ERROR => {}",err)}
+    };
+    println!("{:?}",&buffer[..]); 
+    Ok(())
+ }
