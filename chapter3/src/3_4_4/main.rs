c3e3e0bb563b2dda6370b08b370e73c91b032a1d
Yuta Hinokuma <yuta.hinokuma725@gmail.com>
2021-05-01 14:05:33 +0900

    io.ReadAtをRustではRead + Seekだと仮定して3_4_4を実装する。

--- chapter3/src/3_4_4/main.rs
+++ chapter3/src/3_4_4/main.rs
@@ -1,3 +1,36 @@
+use std::io::{Cursor, Read, Seek, SeekFrom};
+
+struct StringReader {
+    inner: BytesReader,
+}
+
+impl StringReader {
+    pub fn new(s: &str) -> Self {
+        Self {
+            inner: Cursor::new(s.as_bytes().to_vec()),
+        }
+    }
+}
+
+impl Read for StringReader {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.inner.read(buf)
+    }
+}
+
+impl Seek for StringReader {
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        self.inner.seek(pos)
+    }
+}
+
+type BytesReader = Cursor<Vec<u8>>;
+
 fn main() {
-    unimplemented!("rustにgoのio.ReadAtに相当するものが実質std(要定義)までを含めても見受けられないので実装をスキップ");
+    // BufferはRustではVec<u8>とStringが兼ねていると思っています。
+
+    let b_reader1 = BytesReader::new(vec![0x10, 0x20, 0x30]);
+    let b_reader2 = BytesReader::new("文字列をバイト配列にキャストして設定".as_bytes().to_vec());
+
+    let s_reader = StringReader::new("Readerの出力内容は文字列で渡す");
 }
