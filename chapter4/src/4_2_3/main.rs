60ad488e75dee2cc00e152f447fa92ff9f558726
Yuta Hinokuma <yuta.hinokuma725@gmail.com>
2021-05-01 23:13:56 +0900

    4_2_3を実装する。

---
+++ chapter4/src/4_2_3/main.rs
@@ -0,0 +1,30 @@
+use tokio;
+use tokio::sync::mpsc;
+use tokio::time::{sleep, Duration};
+
+fn prime_number() -> mpsc::Receiver<u64> {
+    let (tx, mut rx) = mpsc::channel(100);
+
+    tokio::spawn(async move {
+        tx.send(2);
+        for i in (3..100000).step_by(2) {
+            let l = (i as f64).sqrt() as u64;
+            let found = (3..=l).step_by(2).into_iter().any(|j| i % j == 0);
+            if !found {
+                tx.send(i).await;
+            }
+        }
+        tx.closed().await; // rustではDropで利用されてるからいらないけど一応
+    });
+
+    rx
+}
+
+#[tokio::main]
+async fn main() {
+    let mut rx = prime_number();
+
+    while let Some(x) = rx.recv().await {
+        println!("{}", x);
+    }
+}
