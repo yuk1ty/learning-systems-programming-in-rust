e072dc91252ae6879973e7ad4e730af0d772ee64
saffron <y.saffrontea@gmail.com>
2021-05-01 18:47:59 +0900

    add 3_2_2_2

---
+++ chapter3/src/main_3_2_2_2.rs
@@ -0,0 +1,29 @@
+/**
+ * 
+ * chapter 3_2_2_2
+ * io.copy with size
+ * 
+ */
+use std::io::copy;
+use std::io::Read;
+use std::io::Write;
+use std::io::Result;
+
+pub fn main() -> std::io::Result<()>{
+    let mut reader : &[u8] = b"hello!";
+    let mut buffer : Vec<u8> = vec![];
+  match copyn(&mut reader,&mut buffer,3){
+        Ok(result) => result,
+        Err(err) => {panic!("ERROR => {}",err)}
+    };
+    println!("{:?}",&buffer[..]); 
+    Ok(())
+}
+
+fn copyn<R: ?Sized,W: ?Sized>(reader : &mut R,writer :&mut W,size : u64)-> Result<u64>
+where
+  R: Read,
+  W: Write,
+{
+  return copy(&mut reader.take(size),writer);
+}
