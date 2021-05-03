abd12e6afdf89a61cc5a7c14be6ff9c785edcdc4
higumachan <harekumo792154@gmail.com>
2021-05-03 11:46:11 +0900

    3_4_2が出力するファイルの拡張子を変える。

--- chapter3/src/3_4_2/main.rs
+++ chapter3/src/3_4_2/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 fn main() {
     let mut src_file = File::open("main.rs").unwrap();
 
-    let mut dest_file = File::create("was_copied.rs").unwrap();
+    let mut dest_file = File::create("was_copied.txt").unwrap();
 
     std::io::copy(&mut src_file, &mut dest_file).unwrap();
 }
--- chapter3/src/3_4_2/was_copied.rs
+++
@@ -1,9 +0,0 @@
-use std::fs::File;
-
-fn main() {
-    let mut src_file = File::open("main.rs").unwrap();
-
-    let mut dest_file = File::create("was_copied.rs").unwrap();
-
-    std::io::copy(&mut src_file, &mut dest_file).unwrap();
-}
