877eff12269e5484bb1839ad01912858163e5049
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-05-03 18:11:52 +0900

    add 3.6.2

---
+++ chapter3/src/3_6_2/main.rs
@@ -0,0 +1,20 @@
+/// Rust には scanf 関数はない。したがって、この節は本来は実装できない。
+/// Rust のマクロを使用すると scanf に近い実装を作ることができるので、参考程度に実装しておく。
+/// マクロを自前実装する以外の選択肢としては、下記のようなクレートを使用する手もある。
+/// - [text_io](https://github.com/oli-obk/rust-si): scan マクロが存在する。
+/// - [scan_fmt](https://github.com/wlentz/scan_fmt): 同様に scan_fmt マクロが存在する。
+
+macro_rules! scan {
+    ( $string:expr, $sep:expr, $( $x:ty ),+ ) => {{
+        let mut iter = $string.split($sep);
+        ($(iter.next().map(|word| word.parse::<$x>().ok().expect(&format!("couldn't parse {}", word))).unwrap(),)*)
+    }}
+}
+
+const SOURCE: &str = "123 1.234 1.0e4 test";
+
+fn main() -> std::io::Result<()> {
+    let (i, f, g, s) = scan!(SOURCE, char::is_whitespace, i32, f64, f64, String);
+    println!("i={} f={} g={} s={}", i, f, g, s);
+    Ok(())
+}
