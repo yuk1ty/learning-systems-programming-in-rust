6195a74ee751b9bd554190cc2ee98e339bdfbc61
nasa <htilcs1115@gmail.com>
2021-05-03 16:16:24 +0900

    Update chapter3/src/3_9_2/main.rs
    Co-authored-by: Sho Nakatani <lay.sakura@gmail.com>

--- chapter3/src/3_9_2/main.rs
+++ chapter3/src/3_9_2/main.rs
@@ -3,10 +3,7 @@ use std::io;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
-    let mut bytes = [0; 1024];
-    for byte in bytes.iter_mut() {
-        *byte = rand::random()
-    }
+    let bytes = rand::thread_rng().gen::<[u8; 1024]>();
 
     let mut file = File::create("random.txt")?;
     file.write_all(&bytes)?;
