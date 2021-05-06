9efa8c9773326e21476f3b2a7f81ca366f1e8013
Sho Nakatani <lay.sakura@gmail.com>
2021-05-06 19:18:04 +0900

    signalをtaskが受け取る形に修正

--- chapter4/src/4_3_1/main.rs
+++ chapter4/src/4_3_1/main.rs
@@ -2,13 +2,23 @@ use std::io;
 
 use signal_hook::{consts::SIGINT, iterator::Signals};
 
-fn main() -> io::Result<()> {
-    let mut signals = Signals::new(&[SIGINT])?;
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
 
-    println!("Waiting SIGINT (CTRL+C)");
-    if let Some(_) = signals.into_iter().next() {
-        println!("SIGINT arrived");
-    }
+    tokio::spawn(async move {
+        let mut signals = Signals::new(&[SIGINT])?;
+
+        println!("[signal thread] Waiting for SIGINT (CTRL+C)");
+        if signals.into_iter().next().is_some() {
+            tx.send(()).await.unwrap();
+        }
+        Ok::<(), io::Error>(())
+    });
+
+    println!("[main thread] Waiting for a message from channel");
+    let _ = rx.recv().await;
+    println!("[main thread] Got a message");
 
     Ok(())
 }
