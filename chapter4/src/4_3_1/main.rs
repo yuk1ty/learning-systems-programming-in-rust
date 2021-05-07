874c11c5d2675634d27dc9a930bc6e7024deb642
Sho Nakatani <lay.sakura@gmail.com>
2021-05-08 05:29:34 +0900

    tokioのsignalを使い、signal-hook依存をなくした

--- chapter4/src/4_3_1/main.rs
+++ chapter4/src/4_3_1/main.rs
@@ -1,23 +1,23 @@
 use std::io;
 
-use signal_hook::{consts::SIGINT, iterator::Signals};
+use tokio::signal::unix::{signal, SignalKind};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    let (sigint_tx, mut sigint_rx) = tokio::sync::mpsc::channel::<()>(1);
 
     tokio::spawn(async move {
-        let mut signals = Signals::new(&[SIGINT])?;
+        let mut sig_stream = signal(SignalKind::interrupt())?;
 
         println!("[signal thread] Waiting for SIGINT (CTRL+C)");
-        if signals.into_iter().next().is_some() {
-            tx.send(()).await.unwrap();
-        }
+        sig_stream.recv().await;
+
+        sigint_tx.send(()).await.unwrap();
         Ok::<(), io::Error>(())
     });
 
     println!("[main thread] Waiting for a message from channel");
-    let _ = rx.recv().await;
+    let _ = sigint_rx.recv().await;
     println!("[main thread] Got a message");
 
     Ok(())
