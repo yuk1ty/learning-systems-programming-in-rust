962a6fb42174aca3376f25870016b41efd0492dc
kento tajiri <k.denjiry@gmail.com>
2021-05-08 16:27:04 +0900

    example work correctly

--- chapter10/src/10_1/main.rs
+++ chapter10/src/10_1/main.rs
@@ -1,3 +1,23 @@
+use notify::{watcher, RecursiveMode, Watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 fn main() {
-    println!("hello");
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(".", RecursiveMode::Recursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
 }
