ba7efd2716be725da77f0630bc0a831d480734be
Sho Nakatani <lay.sakura@gmail.com>
2021-05-11 14:38:32 +0900

    別crateに切り出した

---
+++ chapter10_2_asm_syscall/src/main.rs
@@ -0,0 +1,31 @@
+#![feature(asm)]
+
+mod linux;
+
+use linux::arch::x86_64::fcntl;
+use std::{thread, time};
+
+fn main_linux_x86_64() -> linux::Result<()> {
+    let fd = fcntl::open_readonly("Cargo.toml")?;
+
+    println!("try locking...");
+    fcntl::flock(&fd, fcntl::FlockOperation::LockExclusive)?;
+    println!("locked!");
+
+    thread::sleep(time::Duration::from_secs(10));
+
+    fcntl::flock(&fd, fcntl::FlockOperation::Unlock)?;
+    println!("unlock");
+
+    Ok(())
+}
+
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+fn main() {
+    main_linux_x86_64()
+}
+
+#[cfg(not(all(target_os = "linux", target_arch = "x86_64")))]
+fn main() {
+    panic!("Linux ABI, x86_64 ターゲットの `syscall` アセンブリを使うプログラムです。クロスコンパイルにも対応していません。")
+}
