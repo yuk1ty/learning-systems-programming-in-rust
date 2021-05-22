8aaaaf8d11d030ba134d6893a4aa44cfb62737bb
Shinichi Morimoto <shnmorimoto@gmail.com>
2021-05-22 21:30:27 +0900

    add 9_5_7

---
+++ chapter9/src/9_5_7/main.rs
@@ -0,0 +1,36 @@
+use lib::path::PathError;
+use std::ffi::OsStr;
+use std::{collections::HashSet, env};
+use walkdir::WalkDir;
+
+fn main() -> Result<(), PathError> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!(
+            "Find images
+
+Usage:
+    {} [path to find]",
+            &args[0]
+        );
+        std::process::exit(0);
+    }
+    let root = &args[1];
+    let image_suffix: HashSet<&OsStr> = ["jpg", "jpeg", "png", "webp", "gif", "tiff", "eps"]
+        .iter()
+        .map(|str| OsStr::new(str))
+        .collect();
+    for path in WalkDir::new(root)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|f| !f.file_type().is_dir())
+        .filter(|f| {
+            let extention = f.path().extension().unwrap_or_else(|| OsStr::new(""));
+            image_suffix.contains(extention)
+        })
+    {
+        println!("{}", path.into_path().display());
+    }
+
+    Ok(())
+}
