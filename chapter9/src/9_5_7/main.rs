77e1b0c6d9a5b4dcb6fcbdae79493d5400a0bdac
Shinichi Morimoto <shnmorimoto@gmail.com>
2021-05-23 20:56:33 +0900

    fix to not use OsStr

--- chapter9/src/9_5_7/main.rs
+++ chapter9/src/9_5_7/main.rs
@@ -1,5 +1,4 @@
 use lib::path::PathError;
-use std::ffi::OsStr;
 use std::{collections::HashSet, env};
 use walkdir::WalkDir;
 
@@ -16,18 +15,24 @@ Usage:
         std::process::exit(0);
     }
     let root = &args[1];
-    let image_suffix: HashSet<&OsStr> = ["jpg", "jpeg", "png", "webp", "gif", "tiff", "eps"]
+    let image_suffix: HashSet<&'static str> = ["jpg", "jpeg", "png", "webp", "gif", "tiff", "eps"]
         .iter()
-        .map(|str| OsStr::new(str))
+        .cloned()
         .collect();
-    // ディレクトリのトラバースのためにworkdir crateを利用する
+    // ディレクトリのトラバースのためにwalkdir crateを利用する
     // ディレクトリ、拡張子が合わないものについては、フィルタして除外する
+    // walkdir crateでは指定のディレクトリをスキップするような機能は存在しない
     for path in WalkDir::new(root)
         .into_iter()
         .filter_map(Result::ok)
         .filter(|f| !f.file_type().is_dir())
         .filter(|f| {
-            let extention = f.path().extension().unwrap_or_else(|| OsStr::new(""));
+            let extention_opt = f
+                .path()
+                .extension()
+                .and_then(|ext| ext.to_str())
+                .map(|ext| ext.to_lowercase());
+            let extention = extention_opt.as_deref().unwrap_or("");
             image_suffix.contains(extention)
         })
     {
