b14306f64f7c9333fcb89ed371fab84775655f74
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-05 16:53:10 +0900

    add 11_1_6

--- chapter11/src/11_1_6/main.rs
+++ chapter11/src/11_1_6/main.rs
@@ -1,3 +1,9 @@
+use nix::unistd::getcwd;
+use std::env::current_dir;
+
 fn main() {
-    println!("11_1_6");
+    // nix クレートを使用する場合は getcwd が使える。
+    println!("{}", getcwd().unwrap().to_str().unwrap());
+    // あるいは、Rust の std::env にある current_dir を使ってもよい。
+    println!("{}", current_dir().unwrap().to_str().unwrap());
 }
