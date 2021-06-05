558ee887b8b82508e1d9ef9787bd61155eb6b12a
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-06-05 16:46:35 +0900

    add annotation not compiling on apple platform

--- chapter11/src/11_1_4/main.rs
+++ chapter11/src/11_1_4/main.rs
@@ -1,3 +1,4 @@
+#[cfg(not(any(target_os = "ios", target_os = "macos")))]
 use nix::unistd::getgroups;
 use nix::unistd::{getgid, getuid};
 
@@ -6,6 +7,12 @@ fn main() {
     println!("グループID: {}", getgid());
 
     // nix クレートにおける getgroups は Apple のプラットフォームでは実行できない。
+    #[cfg(not(any(target_os = "ios", target_os = "macos")))]
+    get_groups();
+}
+
+#[cfg(not(any(target_os = "ios", target_os = "macos")))]
+fn get_groups() {
     let groups = getgroups().unwrap();
     println!("サブグループID: {:?}", groups);
 }
